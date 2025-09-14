748e06ffbe89f0908b08f5c759c9274f86e930b1
bmsbharath <bmsbharath201@gmail.com>
2025-09-15 00:07:28 +0530

    added echo message after started application

--- groovyfile.groovy
+++ groovyfile.groovy
@@ -1,8 +1,9 @@
 def build(){
-    if (fileExists('target')) {
+   /* if (fileExists('target')) {
         echo 'Maven build already exists, skipping mvn clean install'
     } else {
         sh 'mvn clean package -DskipTests'
-    }
+    }*/
+    sh 'mvn clean package -DskipTests'
 }
 return this
